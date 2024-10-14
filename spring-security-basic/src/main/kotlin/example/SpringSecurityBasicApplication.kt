fb3441055149ec946d3bbe1f2f9a644d7ff04e5b
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2024-10-14 17:00:23 +0900

    main.kt 네이밍 모듈과 통일

--- spring-security-basic/src/main/kotlin/example/SpringSecurityBasicApplication.kt
+++ spring-security-basic/src/main/kotlin/example/SpringSecurityBasicApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringSecurityWorksServletApplication
+class SpringSecurityBasicApplication
 
 fun main(args: Array<String>) {
-    runApplication<SpringSecurityWorksServletApplication>(*args)
+    runApplication<SpringSecurityBasicApplication>(*args)
 }
--- spring-security-basic/src/test/kotlin/example/SpringSecurityBasicApplicationTests.kt
+++ spring-security-basic/src/test/kotlin/example/SpringSecurityBasicApplicationTests.kt
@@ -4,10 +4,8 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringSecurityWorksServletApplicationTests {
-
+class SpringSecurityBasicApplicationTests {
     @Test
     fun contextLoads() {
     }
-
 }
